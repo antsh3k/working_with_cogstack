983379f64c33e6693be3ed52c737cce90f910acd
Anthony Shek <antsh3k@users.noreply.github.com>
2022-03-29 10:07:06 +0100

    vocab file fixed

--- medcat/1_create_model/create_vocab/create_vocab.py
+++ medcat/1_create_model/create_vocab/create_vocab.py
@@ -1,13 +1,14 @@
 from medcat.vocab import Vocab
+import os
 
 vocab = Vocab()
 
-MODEL_DIR = "./models/"
+model_dir = "data/medcat_models/vocab"
 
 # the vocab.txt file need to be in the tab sep format: <token>\t<word_count>\t<vector_embedding_separated_by_spaces>
 # Current vocab uses pre-calculated vector embedding from Word2Vec, future use embeddings calculated from BERT tokeniser
 # embeddings of 300 dimensions is standard
 
-vocab.add_words('vocab_data.txt', replace=True)
+vocab.add_words(os.path.join(model_dir, 'vocab_data.txt'), replace=True)
 vocab.make_unigram_table()
-vocab.save(MODEL_DIR + "basic_vocab.dat")
+vocab.save(os.path.join(model_dir + "vocab.dat"))
